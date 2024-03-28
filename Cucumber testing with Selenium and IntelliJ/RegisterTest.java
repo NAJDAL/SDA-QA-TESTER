be4c6fc6fe5a3376c3889d60c6c65ad1f9dd9b34
NAJDAL <88602467+NAJDAL@users.noreply.github.com>
2024-03-28 03:29:51 +0300

    Update and rename New Text Document.txt to RegisterTest.java

---
+++ Cucumber testing with Selenium and IntelliJ/RegisterTest.java
@@ -0,0 +1,46 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+//Task01-FluentPOM
+ 
+            /*
+            go to url : https://ecommerce-playground.lambdatest.io/index.php?route=account/register
+            register account test with fluent page object approach
+         */
+
+public class RegisterTest {
+
+    private WebDriver driver;
+    private RegisterPage registerPage;
+
+    @BeforeClass
+    public void setUp() {
+        driver = new ChromeDriver();
+        registerPage = new RegisterPage(driver);
+    }
+
+    @Test
+    public void registerAccountTest() {
+        registerPage.navigateToRegistrationPage();
+        registerPage.enterFirstName("John");
+        registerPage.enterLastName("Doe");
+        registerPage.enterEmail("john.doe@example.com");
+        registerPage.enterTelephone("1234567890");
+        registerPage.enterPassword("password123");
+        registerPage.confirmPassword("password123");
+        registerPage.agreeToPrivacyPolicy();
+        registerPage.clickContinueButton();
+        
+        String successMessage = registerPage.getRegistrationSuccessMessage();
+        Assert.assertTrue(successMessage.contains("Your Account Has Been Created!"));
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+}
