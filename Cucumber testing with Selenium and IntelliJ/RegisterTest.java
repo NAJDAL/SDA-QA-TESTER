24b9f7e197f88c3049254840206f58bd2b56f153
NAJDAL <88602467+NAJDAL@users.noreply.github.com>
2024-03-28 03:32:36 +0300

    Update RegisterTest.java

--- Cucumber testing with Selenium and IntelliJ/RegisterTest.java
+++ Cucumber testing with Selenium and IntelliJ/RegisterTest.java
@@ -1,9 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.openqa.selenium.WebElement;
 
 //Task01-FluentPOM
 
@@ -13,35 +10,78 @@ import org.testng.annotations.Test;
             register account test with fluent page object approach
          */
 
-public class RegisterTest {
+
+
+public class FluentRegisterPage {
 
     private WebDriver driver;
-    private RegisterPage registerPage;
 
-    @BeforeClass
-    public void setUp() {
-        driver = new ChromeDriver();
-        registerPage = new RegisterPage(driver);
+    public FluentRegisterPage(WebDriver driver) {
+        this.driver = driver;
     }
 
-    @Test
-    public void registerAccountTest() {
-        registerPage.navigateToRegistrationPage();
-        registerPage.enterFirstName("John");
-        registerPage.enterLastName("Doe");
-        registerPage.enterEmail("john.doe@example.com");
-        registerPage.enterTelephone("1234567890");
-        registerPage.enterPassword("password123");
-        registerPage.confirmPassword("password123");
-        registerPage.agreeToPrivacyPolicy();
-        registerPage.clickContinueButton();
-        
-        String successMessage = registerPage.getRegistrationSuccessMessage();
-        Assert.assertTrue(successMessage.contains("Your Account Has Been Created!"));
+    public FluentRegisterPage navigateToRegistrationPage() {
+        driver.get("https://ecommerce-playground.lambdatest.io/index.php?route=account/register");
+        return this;
     }
 
-    @AfterClass
-    public void tearDown() {
-        driver.quit();
+    public FluentRegisterPage enterFirstName(String firstName) {
+        WebElement firstNameInput = driver.findElement(By.id("input-firstname"));
+        firstNameInput.clear();
+        firstNameInput.sendKeys(firstName);
+        return this;
+    }
+
+    public FluentRegisterPage enterLastName(String lastName) {
+        WebElement lastNameInput = driver.findElement(By.id("input-lastname"));
+        lastNameInput.clear();
+        lastNameInput.sendKeys(lastName);
+        return this;
+    }
+
+    public FluentRegisterPage enterEmail(String email) {
+        WebElement emailInput = driver.findElement(By.id("input-email"));
+        emailInput.clear();
+        emailInput.sendKeys(email);
+        return this;
+    }
+
+    public FluentRegisterPage enterTelephone(String telephone) {
+        WebElement telephoneInput = driver.findElement(By.id("input-telephone"));
+        telephoneInput.clear();
+        telephoneInput.sendKeys(telephone);
+        return this;
+    }
+
+    public FluentRegisterPage enterPassword(String password) {
+        WebElement passwordInput = driver.findElement(By.id("input-password"));
+        passwordInput.clear();
+        passwordInput.sendKeys(password);
+        return this;
+    }
+
+    public FluentRegisterPage confirmPassword(String confirmPassword) {
+        WebElement confirmPasswordInput = driver.findElement(By.id("input-confirm"));
+        confirmPasswordInput.clear();
+        confirmPasswordInput.sendKeys(confirmPassword);
+        return this;
+    }
+
+    public FluentRegisterPage agreeToPrivacyPolicy() {
+        WebElement privacyPolicyCheckbox = driver.findElement(By.name("agree"));
+        if (!privacyPolicyCheckbox.isSelected()) {
+            privacyPolicyCheckbox.click();
+        }
+        return this;
+    }
+
+    public FluentRegisterPage clickContinueButton() {
+        driver.findElement(By.cssSelector("input[type='submit'][value='Continue']")).click();
+        return this;
+    }
+
+    public void verifyRegistrationSuccessMessage() {
+        WebElement successMessage = driver.findElement(By.cssSelector(".alert-success"));
+        assert successMessage.isDisplayed() : "Registration success message not displayed";
     }
 }
